679c2b5edbcadb1381c5944ac8589cad4af05e14
auroraptor <interestingbutno@yandex.ru>
2022-11-15 14:52:11 +0300

    fix: убрать лог

--- frontend/src/components/ProtectedRoute.js
+++ frontend/src/components/ProtectedRoute.js
@@ -1,7 +1,6 @@
 import { Outlet, Navigate } from "react-router-dom";
 
 function ProtectedRoute({onLogin}) {
-  console.log('[AUTHENTICATION PROTECTION]: ', onLogin);
   return (
     onLogin ? <Outlet /> : <Navigate to="/signin"/>
   )
