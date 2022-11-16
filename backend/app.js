6104fc69a2a1bec7ceb6bfb1ff16529f3ca2d566
auroraptor <interestingbutno@yandex.ru>
2022-11-16 20:54:29 +0300

    feat: добавить краш-тест сервера

--- backend/app.js
+++ backend/app.js
@@ -54,6 +54,12 @@ app.use((req, res, next) => {
 
 const { PORT = 3000 } = process.env;
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.post('/signin', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
