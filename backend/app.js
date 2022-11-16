5ed6f4f2f92fb94b345db7f34e65675453b50605
auroraptor <interestingbutno@yandex.ru>
2022-11-16 12:10:19 +0300

    feat: разрешает браузеру запросы из любого источника

--- backend/app.js
+++ backend/app.js
@@ -42,7 +42,7 @@ app.use((req, res, next) => {
   const requestHeaders = req.headers['access-control-request-headers'];
 
   if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Credentials', true);
   }
 
