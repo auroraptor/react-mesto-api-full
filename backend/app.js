f651b3ff8c07074b1e3378062c268571960b62f7
auroraptor <interestingbutno@yandex.ru>
2022-11-16 12:52:47 +0300

    feat: установить заголовок, который разрешает браузеру запросы

--- backend/app.js
+++ backend/app.js
@@ -32,21 +32,21 @@ const allowedCors = [
   'http://localhost:3000',
   'https://auro.nomoredomains.icu',
   'https://api.auro.nomoredomains.icu',
+  'https://api.auro.nomoredomains.icu/users/me',
 ];
 
 app.use((req, res, next) => {
   const { origin } = req.headers;
-  const { method } = req;
 
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
   const requestHeaders = req.headers['access-control-request-headers'];
 
   if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', origin);
     res.header('Access-Control-Allow-Credentials', true);
   }
 
-  if (method === 'OPTIONS') {
+  if (req.method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
     return res.end();
