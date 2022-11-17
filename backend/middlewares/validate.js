bcf9dc97516e5500999725c62c96c0d51e1b3ec8
auroraptor <interestingbutno@yandex.ru>
2022-11-17 12:43:28 +0300

    fix: перенести валидацию user

--- backend/middlewares/validate.js
+++ backend/middlewares/validate.js
@@ -1,44 +1,52 @@
 const { celebrate, Joi } = require('celebrate');
 const { url } = require('../utils/regexps');
 
-module.exports.validateUserBody = (req, res, next) => {
-  celebrate({
-    body: Joi.object().keys({
-      name: Joi.string().min(2).max(30),
-      about: Joi.string().min(2).max(30),
-      avatar: Joi.string().pattern(url),
-      email: Joi.string().required().email(),
-      password: Joi.string().required().min(8),
-    }),
-  });
-  next();
-};
+module.exports.validateUserBody = celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().pattern(url),
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+  }),
+});
 
-module.exports.validateAuth = (req, res, next) => {
-  celebrate({
-    body: Joi.object().keys({
-      email: Joi.string().required().email(),
-      password: Joi.string().required().min(8),
-    }),
-  });
-  next();
-};
+module.exports.validateAuth = celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+  }),
+});
 
-module.exports.validateCard = (req, res, next) => {
-  celebrate({
-    body: Joi.object().keys({
-      name: Joi.string().required().min(2).max(30),
-      link: Joi.string().required().pattern(url),
-    }),
-  });
-  next();
-};
+module.exports.validateUserId = celebrate({
+  params: Joi.object().keys({
+    id: Joi.string().pattern(/[a-f0-9]{24,24}/).length(24),
+  }),
+});
 
-module.exports.validateCardId = (req, res, next) => {
-  celebrate({
-    params: Joi.object().keys({
-      cardId: Joi.string().pattern(/[a-f0-9]{24,24}/).length(24),
-    }),
-  });
-  next();
-};
+module.exports.validateUser = celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().pattern(url),
+  }),
+});
+
+module.exports.validateAvatar = celebrate({
+  body: Joi.object().keys({
+    avatar: Joi.string().pattern(url),
+  }),
+});
+
+module.exports.validateCard = celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    link: Joi.string().required().pattern(url),
+  }),
+});
+
+module.exports.validateCardId = celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().pattern(/[a-f0-9]{24,24}/).length(24),
+  }),
+});
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,31 +1,13 @@
 const router = require('express').Router();
-const { celebrate, Joi } = require('celebrate');
+const { validateUserId, validateAvatar, validateUser } = require('../middlewares/validate');
 const {
   getUsers, getUserById, updateAvatar, updateUser, getCurrentUser,
 } = require('../controllers/users');
-const { url, id } = require('../utils/regexps');
 
 router.get('/me', getCurrentUser);
 router.get('/', getUsers);
-
-router.get('/:id', celebrate({
-  params: Joi.object().keys({
-    id: Joi.string().pattern(id).length(24),
-  }),
-}), getUserById);
-
-router.patch('/me', celebrate({
-  body: Joi.object().keys({
-    name: Joi.string().min(2).max(30),
-    about: Joi.string().min(2).max(30),
-    avatar: Joi.string().pattern(url),
-  }),
-}), updateUser);
-
-router.patch('/me/avatar', celebrate({
-  body: Joi.object().keys({
-    avatar: Joi.string().pattern(url),
-  }),
-}), updateAvatar);
+router.get('/:id', validateUserId, getUserById);
+router.patch('/me', validateUser, updateUser);
+router.patch('/me/avatar', validateAvatar, updateAvatar);
 
 module.exports = router;
