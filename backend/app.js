6045da5a847f048c1f9617eedda1f37df7113da0
auroraptor <interestingbutno@yandex.ru>
2022-11-17 11:41:30 +0300

    feat: rateLimit

--- backend/app.js
+++ backend/app.js
@@ -2,10 +2,11 @@ require('dotenv').config();
 
 const express = require('express');
 const mongoose = require('mongoose');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
-const helmet = require('helmet');
 const routes = require('./routes');
 const cors = require('./middlewares/cors');
 const { logNow, logError } = require('./utils/log');
@@ -26,6 +27,14 @@ mongoose.connect('mongodb://localhost:27017/mestodb', { autoIndex: true })
   .catch((err) => logError(err));
 
 const { PORT = 3000 } = process.env;
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
+app.use(limiter);
 
 app.get('/crash-test', () => {
   setTimeout(() => {
